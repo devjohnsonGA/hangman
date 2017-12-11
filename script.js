c1639992cba703b93f6a433da1c753215485b48b
Devon Johnson <dseth.johnson@GMAIL.COM>
2017-12-11 16:10:13 -0500

    added even prevent default to stop auto refresh from button

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // alert("Welcome to HangMan ...");
 
-document.getElementById('submit').addEventListener('click', function () {
+document.getElementById('submit').addEventListener('click', function (event) {
+  event.preventDefault()
   var guess = document.getElementById('text').value
   console.log(guess)
-  document.getElementById('word').innerHTML = $('word').data(guess)
+  document.getElementById('word').innerHTML = guess
 })
+
+// retrieve data from botton then display it
+// once displayed figure out how to split it up then show blank spaces
+// work on guessing functionality
