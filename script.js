9a53a12348b9b8ef1a2bdd12debc308179d8e122
Devon Johnson <dseth.johnson@GMAIL.COM>
2017-12-11 16:09:29 -0500

    jquery added and trying to figure out dom manipulation

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // alert("Welcome to HangMan ...");
 
-document.getElementById("submit").addEventListener("click", function(){
-  alert(document.getElementById("text").value);
-});
+document.getElementById('submit').addEventListener('click', function () {
+  var guess = document.getElementById('text').value
+  console.log(guess)
+  document.getElementById('word').innerHTML = $('word').data(guess)
+})
