8b63dffcc480a91a24b194c6a27f5cd724537cb5
Devon Johnson <dseth.johnson@GMAIL.COM>
2017-12-12 12:14:22 -0500

    guessing added

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-// alert("Welcome to HangMan ...");
+alert("Welcome to HangMan ...");
+var word = ['T', 'E', 'S', 'T']
+var hangman = ['H', 'A', 'N', 'G', 'M', 'A', 'N']
+var guess = prompt("Enter another letter")
+for (i=0; i<8; i++){
+  if( word[] == word[]) {
+    alert("That was a correct letter")
+  } else {
+    alert(hangman[i])
+  }
+}
+//guessing game element to cycle through HANGMAN
+
 
 document.getElementById('submit').addEventListener('click', function (event) {
   event.preventDefault()
-  var guess = document.getElementById('text').value
+  var guess = document.getElementById('text').value.split('')
   console.log(guess)
   document.getElementById('word').innerHTML = guess
 })
