f41d9d439fb64464abe5b0a4b6400e605a7f943e
Devon Johnson <dseth.johnson@GMAIL.COM>
2017-12-12 15:54:18 -0500

    new approach

--- script.js
+++ script.js
@@ -1,23 +1,63 @@
-alert("Welcome to HangMan ...");
-var word = ['T', 'E', 'S', 'T']
-var hangman = ['H', 'A', 'N', 'G', 'M', 'A', 'N']
-var guess = prompt("Enter another letter")
-for (i=0; i<8; i++){
-  if( word[] == word[]) {
-    alert("That was a correct letter")
-  } else {
-    alert(hangman[i])
-  }
-}
+//set up guessing game
+//word is set but would want to add random word selector
+//split it into 2 player with a successful guess leading to solve opportunity
+//unsucessful guess is tallied to see who gets hangman first out of 2 players
+//if both players get hangman reveal Word
+
+
+document.getElementById('submit').addEventListener('click', function(){
+  event.preventDefault()
+  alert("Welcome to HangMan. Submit a guess in box!");
+})
+
+document.getElementById('submit').addEventListener('click', function(){
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+// // alert("Welcome to HangMan. Submit a guess in box!");
+//
+// function hangmanGuess(){
+// var word = ['T', 'E', 'S', 'T']
+// var hangman = ['H', 'A', 'N', 'G', 'M', 'A', 'N']
+// var guessAgain = alert("Enter another letter")
+// var guess = document.getElementById('text')
+// var letter = guess
+// for (i=0; i<8; i++){
+//   if( letter == word) {
+//     event.preventDefault()
+//     console.log(letter)
+//     console.log(word)
+//     // alert("That was a correct letter")
+//     // alert(word[] + " is a corret guess!")
+//   } else {
+//     console.log(letter)
+//     // alert(guessAgain)
+//     // alert(letter + " is an incorrect guess!")
+//   }
+//   }
+// })
+
+
+
 //guessing game element to cycle through HANGMAN
 
 
-document.getElementById('submit').addEventListener('click', function (event) {
-  event.preventDefault()
-  var guess = document.getElementById('text').value.split('')
-  console.log(guess)
-  document.getElementById('word').innerHTML = guess
-})
+// document.getElementById('submit').addEventListener('click', function (event) {
+//   event.preventDefault()
+//   var guess = document.getElementById('text').value.split('')
+//   console.log(guess)
+//   document.getElementById('word').innerHTML = guess
+// })
 
 // retrieve data from botton then display it
 // once displayed figure out how to split it up then show blank spaces
